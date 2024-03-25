fdec0a09881d7427b9039e4c05fc3fbe777b0100
Rapcha01 <jackyrapcha1301@gmail.com>
2024-03-25 15:22:35 +0300

    finish working on Navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,30 +1,47 @@
-
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-export const Navbar = () => {
-    return (
-    
-        <nav className ={styles.navbar}>
-        <a  className ={styles.title} href="/">Portfolio</a>
-    
-    <div className= {styles.menu}>
-        <ul className = {styles.menuItems}>
-            <li>
-                <a href="#about">About</a>
-            </li>
-            <li>
-                <a href="#experience">Experience</a>
-            </li>
-            <li>
-                <a href="#project">Project</a>
-            </li>
-            <li>
-                <a href="#contact">Contact</a>
-            </li>
-        </ul>
-    </div>
-    </nav>
-   
+import { getImageUrl } from "../../utils";
 
-    );
+export const Navbar = () => {
+  const [usetate, setUseState] = useState();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("/nav/closeIcon.png")
+              : getImageUrl("/nav/menuIcon.png")
+          }
+          alt="menu-btn"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        ;
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
